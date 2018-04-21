1a1b5b9ad2c5cae4c4b7604a41cce72cd92a66f4
Quixatocs <quixatocs@gmail.com>
2018-04-21 05:33:56 +0100

    Camera Follow

---
+++ Assets/Scripts/CameraMovementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour {
+
+    public Transform target;
+    public float yOffset;
+    public float cameraSmoothTime = 0.15f;
+
+    private Vector3 velocity = Vector3.zero;
+ 
+    void Update () {
+
+        Vector3 destination = new Vector3(target.position.x, target.position.y + yOffset, target.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, cameraSmoothTime);
+    }
+}
